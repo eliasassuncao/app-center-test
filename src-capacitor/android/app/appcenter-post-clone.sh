29cb8fede69388938241c5faecfd649928270e2e
Elias Gabriel de Carvalho Assunção <elias.assuncao@bild.com.br>
2020-11-09 15:08:58 -0300

    appcenter-post-clone

---
+++ src-capacitor/android/app/appcenter-post-clone.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+# Required nodeJS version
+NODE_VERSION=12.18.3
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node10 "$NODE_VERSION"
+
+# go to root of project
+cd ../../..
+
+# install dependencies
+npm i
+
+# run optimized production build
+npx quasar build -m capacitor -T android
+
+# copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
+# cd src-capacitor
+# npx cap copy
